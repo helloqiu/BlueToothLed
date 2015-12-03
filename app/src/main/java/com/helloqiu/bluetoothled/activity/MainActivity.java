ba6f0801ade908b3f9782ff04634faca0e27dc51
Yuyang Zhang <695441493@qq.com>
2015-12-03 13:03:38 +0800

    init project

---
+++ app/src/main/java/com/helloqiu/bluetoothled/activity/MainActivity.java
@@ -0,0 +1,19 @@
+package com.helloqiu.bluetoothled.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.helloqiu.bluetoothled.R;
+
+/**
+ * Created by helloqiu on 15/12/3.
+ */
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_main_activity);
+
+    }
+}
