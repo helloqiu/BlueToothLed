7adb13e0808386db0c0bbfb8d11333e2b8360813
Yuyang Zhang <695441493@qq.com>
2016-01-20 15:22:06 +0800

    change to normal menu

--- app/src/main/java/com/helloqiu/bluetoothled/activity/MainActivity.java
+++ app/src/main/java/com/helloqiu/bluetoothled/activity/MainActivity.java
@@ -1,25 +1,18 @@
 package com.helloqiu.bluetoothled.activity;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.LinearLayout;
 
 import com.helloqiu.bluetoothled.R;
 import com.helloqiu.bluetoothled.component.Dot;
-import com.yalantis.contextmenu.lib.ContextMenuDialogFragment;
-import com.yalantis.contextmenu.lib.MenuObject;
-import com.yalantis.contextmenu.lib.MenuParams;
-import com.yalantis.contextmenu.lib.interfaces.OnMenuItemClickListener;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.lang.reflect.Method;
 
 /**
  *
@@ -29,15 +22,11 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     Dot[][] dot;
-    private ContextMenuDialogFragment mMenuDialogFragment;
-    private android.support.v4.app.FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_main_activity);
-        fragmentManager = getSupportFragmentManager();
-        initMenu();
         initLayout();
     }
     private void initLayout(){
@@ -71,88 +60,51 @@ public class MainActivity extends AppCompatActivity {
             linearLayout.addView(linearLayoutHeng[i]);
         }
     }
-    private void initMenu(){
-        MenuObject turnOn = new MenuObject(getResources().getString(R.string.turn_on_all));
-        turnOn.setResource(R.drawable.ic_done_all_black_48dp);
-        turnOn.setBgColor(Color.TRANSPARENT);
-
-        MenuObject turnOff = new MenuObject(getResources().getString(R.string.turn_off_all));
-        turnOff.setResource(R.drawable.icon_turn_off);
-        turnOff.setBgColor(Color.TRANSPARENT);
-
-        MenuObject send = new MenuObject(getResources().getString(R.string.send));
-        send.setResource(R.drawable.icon_bluetooth);
-        send.setBgColor(Color.TRANSPARENT);
-
-        MenuObject exit = new MenuObject(getResources().getString(R.string.exit));
-        exit.setResource(R.drawable.icon_exit);
-        exit.setBgColor(Color.TRANSPARENT);
-
-        List<MenuObject> menuObjects = new ArrayList<>();
-        menuObjects.add(turnOn);
-        menuObjects.add(turnOff);
-        menuObjects.add(send);
-        menuObjects.add(exit);
-
-        DisplayMetrics dm = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(dm);
-        int screenHeight = dm.heightPixels;
-
-        MenuParams menuParams = new MenuParams();
-        menuParams.setActionBarSize(screenHeight / 12);
-        menuParams.setMenuObjects(menuObjects);
-        menuParams.setClosableOutside(true);
-        mMenuDialogFragment = ContextMenuDialogFragment
-                .newInstance(menuParams);
-        mMenuDialogFragment.setItemClickListener(listener);
-    }
-    private OnMenuItemClickListener listener = new OnMenuItemClickListener() {
-        @Override
-        public void onMenuItemClick(View clickedView, int position) {
-            switch (position){
-                case 0:
-                    // turn on all
-                    for (int i = 0 ; i < 16 ; i ++){
-                        for (int j = 0 ; j < 16 ; j++){
-                            dot[i][j].setState(Dot.TURN_ON);
-                        }
-                    }
-                    break;
-                case 1:
-                    //turn off all
-                    for (int i = 0 ; i < 16 ; i ++){
-                        for (int j = 0 ; j < 16 ; j++){
-                            dot[i][j].setState(Dot.TURN_OFF);
-                        }
-                    }
-                    break;
-                case 2:
-                    //send
-                    //TODO: Send data to bluetooth
-
-                    break;
-                case 3:
-                    // exit
-                    android.os.Process.killProcess(android.os.Process.myPid());
-                    System.exit(0);
-            }
-        }
-    };
     @Override
     public boolean onCreateOptionsMenu(final Menu menu) {
+        setIconEnable(menu , true);
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_main, menu);
         return true;
+    }
+    private void setIconEnable(Menu menu, boolean enable){
+        try{
+            Class<?> clazz = Class.forName("android.support.v7.view.menu.MenuBuilder");
+            Method m = clazz.getDeclaredMethod("setOptionalIconsVisible", boolean.class);
+            m.setAccessible(true);
+            m.invoke(menu, enable);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.context_menu:
-                if (fragmentManager.findFragmentByTag(ContextMenuDialogFragment.TAG) == null) {
-                    mMenuDialogFragment.show(fragmentManager, ContextMenuDialogFragment.TAG);
+            case R.id.main_menu_turn_on_all:
+                // turn on all
+                for (int i = 0 ; i < 16 ; i ++){
+                    for (int j = 0 ; j < 16 ; j++){
+                        dot[i][j].setState(Dot.TURN_ON);
+                    }
                 }
                 break;
+            case R.id.main_menu_turn_off_all:
+                //turn off all
+                for (int i = 0 ; i < 16 ; i ++){
+                    for (int j = 0 ; j < 16 ; j++){
+                        dot[i][j].setState(Dot.TURN_OFF);
+                    }
+                }
+                break;
+            case R.id.main_menu_send:
+                //send
+                //TODO : send the data to bluetooth
+                break;
+            case R.id.main_menu_exit:
+                // exit
+                android.os.Process.killProcess(android.os.Process.myPid());
+                System.exit(0);
         }
         return super.onOptionsItemSelected(item);
     }
